4e45d9df6dca11021a01f36078861096a5be04fc
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-27 07:56:48 +0700

    Update audiofix.sh

--- audiofix.sh
+++ audiofix.sh
@@ -2,6 +2,7 @@
 
 wget https://raw.githubusercontent.com/wahasa/nethunter/main/Patch/.audiofix
 
+chmod +x .audiofix
 echo "./.audiofix start" > /usr/local/bin/audiofix
 chmod +x /usr/local/bin/audiofix
 
