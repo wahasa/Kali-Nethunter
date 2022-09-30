2c1e9f3cc7c6f77a7a96d9f46064d10bd2bab7b8
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-30 11:11:16 +0700

    Create xstartupfix.sh

---
+++ xstartupfix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf ~/.vnc/xstartup
+
+wget
+
+echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
+
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vnc-start
+chmod +x /usr/local/bin/vnc-stop
