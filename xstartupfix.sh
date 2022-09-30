1c1832e565dcb7b54c365b3f10dde06b1e8ccd45
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-30 11:13:43 +0700

    Update xstartupfix.sh

--- xstartupfix.sh
+++ xstartupfix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf ~/.vnc/xstartup
 
-wget
+wget https://raw.githubusercontent.com/wahasa/nethunter/main/Patch/xstartup -P ~/.vnc/
 
 echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
 echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
@@ -9,3 +9,5 @@ echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
+
+rm xstartupfix.sh
