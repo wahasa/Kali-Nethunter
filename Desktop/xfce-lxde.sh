fc0905fb8cba02c0ef493fc9d37bfd0a5d001aa3
wahasa <69626847+wahasa@users.noreply.github.com>
2022-10-02 09:45:31 +0700

    Create xfce-lxde.sh

---
+++ Desktop/xfce-lxde.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf ~/.vnc/xstartup
+
+wget https://raw.githubusercontent.com/wahasa/nethunter/main/Patch/xstartup -P ~/.vnc/
+
+echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
+
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vnc-start
+chmod +x /usr/local/bin/vnc-stop
+
+rm xfce-lxde.sh
