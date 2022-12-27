943b9b4006a0eb3c7d4c371e595f18731d2366c3
wahasa <69626847+wahasa@users.noreply.github.com>
2022-12-27 11:32:00 +0700

    Update xstartupfix.sh

--- xstartupfix.sh
+++ xstartupfix.sh
@@ -5,9 +5,17 @@ wget https://raw.githubusercontent.com/wahasa/nethunter/main/Patch/xstartup -P ~
 
 echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
 echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
-
+clear
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
+
+echo " "
+echo "Vnc Server address will run at 127.0.0.1:5901"
+echo " "
+echo "Start Vnc Server, run vnc-start"
+echo " "
+echo "Stop Vnc Server, run vnc-stop"
+echo " "
 
 rm xstartupfix.sh
