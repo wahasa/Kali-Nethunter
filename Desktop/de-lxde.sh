e86dd9c730e0a52b1807f00d576dc58c83c0c234
wahasa <69626847+wahasa@users.noreply.github.com>
2023-06-05 12:38:27 +0700

    Add files via upload

---
+++ Desktop/de-lxde.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#Get the necessary components
+sudo apt update
+sudo apt install udisks2 -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+sudo apt-mark hold udisks2
+sudo apt install sudo -y ; sudo dpkg-reconfigure tzdata
+sudo apt install lxde lxterminal -y
+sudo apt install tigervnc-standalone-server dbus-x11 -y
+mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
+sudo apt --fix-broken install
+sudo apt clean
+
+#Setup the necessary files
+mkdir -p ~/.vnc
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+startlxde" > ~/.vnc/xstartup
+
+echo "#!/bin/sh
+export DISPLAY=:1
+export PULSE_SERVER=127.0.0.1
+rm -rf /run/dbus/dbus.pid
+dbus-launch startlxde" > /usr/local/bin/vncstart
+   echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
+   echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
+   chmod +x ~/.vnc/xstartup
+   chmod +x /usr/local/bin/*
+   clear
+   echo ""
+   echo "Installing Browser,.."
+   echo ""
+#Browser Fix
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/passwd -P .vnc/
+apt install firefox-esr -y
+vnc-start
+sleep 5
+DISPLAY=:1 firefox &
+sleep 10
+pkill -f firefox
+vnc-stop
+sleep 2
+
+wget -O $(find ~/.mozilla/firefox -name *.default-esr)/user.js https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/user.js
+
+rm .vnc/passwd
+   clear
+   echo ""
+   echo "Vnc Server address will run at 127.0.0.1:5901"
+   echo "Start Vnc Server, run vnc-start"
+   echo "Stop Vnc Server, run vnc-stop"
+   echo ""
+rm de-lxde.sh
