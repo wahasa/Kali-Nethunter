01862001429917a6ba12c3d0f08e5c393b563f49
wahasa <69626847+wahasa@users.noreply.github.com>
2023-06-17 12:55:55 +0700

    Add files via upload

---
+++ Switch/xfce-kde.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt install kali-desktop-kde -y
+
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+startplasma-x11" > ~/.vnc/xstartup
+
+echo "#!/bin/sh
+export DISPLAY=:1
+export PULSE_SERVER=127.0.0.1
+rm -rf /run/dbus/dbus.pid
+dbus-launch startplasma-x11" > /usr/local/bin/vncstart
+
+echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/*
+   clear
+   echo ""
+   echo "Vnc Server address will run at 127.0.0.1:5901"
+   echo "Start Vnc Server, run vnc-start"
+   echo "Stop Vnc Server, run vnc-stop"
+   echo ""
+rm xfce-kde.sh
