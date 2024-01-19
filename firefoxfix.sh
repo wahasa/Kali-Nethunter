dd6920920f2eaaa53f2b12b4f31988aff8164839
wahasa <69626847+wahasa@users.noreply.github.com>
2024-01-19 11:06:19 +0700

    firefoxfix.sh

--- firefoxfix.sh
+++ firefoxfix.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+kex
+sleep 5
+DISPLAY=:1 firefox &
+sleep 10
+pkill -f firefox
+kex stop
+sleep 2
+
+wget -O $(find ~/.mozilla/firefox -name *.default-esr)/user.js https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/user.js
+
+rm firefoxfix.sh
