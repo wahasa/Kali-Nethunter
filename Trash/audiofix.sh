fc06c1129d76b7fd2b5ba7e11c104a1c345a97f1
wahasa <69626847+wahasa@users.noreply.github.com>
2024-01-19 10:37:47 +0700

    Delete Trash/audiofix.sh

--- Trash/audiofix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-wget https://raw.githubusercontent.com/wahasa/nethunter/main/Patch/.audiofix
-
-chmod +x .audiofix
-echo "./.audiofix start" > /usr/local/bin/audiofix
-chmod +x /usr/local/bin/audiofix
-
-rm audiofix.sh
