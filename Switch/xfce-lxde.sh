e75108b406a2c81d98ed952a5856bd714df50551
wahasa <69626847+wahasa@users.noreply.github.com>
2024-01-25 12:47:59 +0700

    xfce-lxde.sh

--- Switch/xfce-lxde.sh
+++ Switch/xfce-lxde.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo dpkg-reconfigure tzdata
 sudo apt install dialog kali-desktop-lxde -y
 mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
-
 rm -rf ~/.vnc/xstartup
+
 wget -q https://raw.githubusercontent.com/wahasa/nethunter/main/Patch/xstartup -P ~/.vnc/
 
 echo "#!/bin/sh
@@ -13,7 +13,7 @@ export PULSE_SERVER=127.0.0.1
 rm -rf /run/dbus/dbus.pid
 dbus-launch startlxde" > /usr/local/bin/vncstart
 
-echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
 echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/*
