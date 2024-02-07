803f7b7a50fed7ec7d3ce1a2d2ff266b9a553f95
wahasa <69626847+wahasa@users.noreply.github.com>
2024-02-07 14:32:28 +0700

    xfce-gnome.sh

--- Switch/xfce-gnome.sh
+++ Switch/xfce-gnome.sh
@@ -6,7 +6,7 @@ sudo apt install gnome-session-flashback nautilus gnome-terminal dbus-x11 -y
 rm -rf ~/.vnc/xstartup
 
 wget -q https://raw.githubusercontent.com/wahasa/Kali-Nethunter/main/Patch/xstartup -P ~/.vnc/
-wget -q https://raw.githubusercontent.com/wahasa/Kali-Nethunter/main/Patch/vncstart -P /usr/local/bin/vncstart
+wget -q https://raw.githubusercontent.com/wahasa/Kali-Nethunter/main/Patch/vncstart -P /usr/local/bin/
 
 echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
 echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
