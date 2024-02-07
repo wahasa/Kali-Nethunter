8ad45ddcd248a25be50660c4ece633d354670941
wahasa <69626847+wahasa@users.noreply.github.com>
2024-02-07 12:49:16 +0700

    Update xfce-gnome.sh

--- Switch/xfce-gnome.sh
+++ Switch/xfce-gnome.sh
@@ -5,13 +5,8 @@ sudo apt install dialog dconf-editor gedit plank rofi -y
 sudo apt install gnome-session-flashback nautilus gnome-terminal dbus-x11 -y
 rm -rf ~/.vnc/xstartup
 
-wget -q https://raw.githubusercontent.com/wahasa/nethunter/main/Patch/xstartup -P ~/.vnc/
-
-echo "#!/bin/sh
-export DISPLAY=:1
-export PULSE_SERVER=127.0.0.1
-rm -rf /run/dbus/dbus.pid
-dbus-launch startplasma-x11" > /usr/local/bin/vncstart
+wget -q https://raw.githubusercontent.com/wahasa/Kali-Nethunter/main/Patch/xstartup -P ~/.vnc/
+wget -q https://raw.githubusercontent.com/wahasa/Kali-Nethunter/main/Patch/vncstart -P /usr/local/bin/vncstart
 
 echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
 echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
