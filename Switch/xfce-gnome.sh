b539a0ac53ea4cb99e61b4f67249fbff88cb7741
wahasa <69626847+wahasa@users.noreply.github.com>
2024-02-07 13:44:31 +0700

    Update xfce-gnome.sh

--- Switch/xfce-gnome.sh
+++ Switch/xfce-gnome.sh
@@ -16,3 +16,19 @@ chmod +x /usr/local/bin/*
    echo ""
    echo "Installing Extra Packages,.."
    echo ""
+#Rofi
+cd rofi ; ./setup.sh ; cd
+rm ~/.config/rofi/config.rasi
+rm ~/.config/rofi/launchers/type-3/launcher.sh
+rm ~/.config/rofi/launchers/type-3/style-1.rasi
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/theme/launcher.sh -P ~/.config/rofi/launchers/type-3/
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/theme/style-1.rasi -P ~/.config/rofi/launchers/type-3/
+chmod +x ~/.config/rofi/launchers/type-3/launcher.sh
+rm -rf rofi
+
+#Plank
+wget https://github.com/wahasa/Ubuntu/raw/main/modded/ubuntu/menu.png -P ~/.local/share/icons/
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/plank.desktop -P /usr/share/applications/
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/rofi-launcher.desktop -P /usr/share/applications/
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/plank-preferences.desktop -P /usr/share/applications/
+
