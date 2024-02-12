d13afb1fdfa1f762c30f1cbbbad71389f3819b2f
wahasa <69626847+wahasa@users.noreply.github.com>
2024-02-12 08:57:33 +0700

    xfce-gnome.sh

--- Switch/xfce-gnome.sh
+++ Switch/xfce-gnome.sh
@@ -4,8 +4,9 @@ sudo dpkg-reconfigure tzdata
 sudo apt install dialog dconf-editor git gedit plank rofi -y
 sudo apt install gnome-session-flashback nautilus gnome-terminal dbus-x11 -y
 #sudo apt install gnome budgie-desktop -y
-rm -rf ~/.vnc/xstartup
 
+rm -rf ~/.vnc/xstartup
+rm -rf /usr/local/bin/vncstart
 wget -q https://raw.githubusercontent.com/wahasa/Kali-Nethunter/main/Patch/xstartup -P ~/.vnc/
 wget -q https://raw.githubusercontent.com/wahasa/Kali-Nethunter/main/Patch/vncstart -P /usr/local/bin/
 
@@ -19,16 +20,15 @@ chmod +x /usr/local/bin/*
    echo ""
 #Rofi
 git clone --depth=1 https://github.com/adi1090x/rofi.git
-cd rofi ; ./setup.sh ; cd
 rm ~/.config/rofi/config.rasi
 rm ~/.config/rofi/launchers/type-3/launcher.sh
 rm ~/.config/rofi/launchers/type-3/style-1.rasi
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/theme/launcher.sh -P ~/.config/rofi/launchers/type-3/
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/theme/style-1.rasi -P ~/.config/rofi/launchers/type-3/
 chmod +x ~/.config/rofi/launchers/type-3/launcher.sh
-#rm -rf rofi
 
 #Plank
+git clone --depth=1 https://github.com/adi1090x/rofi.git
 wget https://github.com/wahasa/Ubuntu/raw/main/modded/ubuntu/menu.png -P ~/.local/share/icons/
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/plank.desktop -P /usr/share/applications/
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/rofi-launcher.desktop -P /usr/share/applications/
@@ -44,9 +44,13 @@ wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/org.gnome.Terminal.dockitem -P ~/.config/plank/dock1/launchers/
 
 #Theme
+git clone --depth=1 https://github.com/adi1090x/rofi.git
 mkdir -p ~/.local/share/plank/themes
 mkdir -p ~/.local/share/plank/themes/Azeny
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/theme/dock.theme -P ~/.local/share/plank/themes/Azeny/
+
+cd rofi ; ./setup.sh ; cd
+#rm -rf rofi
    clear
    echo ""
    echo "In Termux"
